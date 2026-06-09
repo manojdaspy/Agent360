b2e2307213ed05ebe225bd7b2ee9b4beb820c34b
Manoj Das <manojdas.py@gmail.com>
2026-06-09 15:48:20 +0545

    content js fixed

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('agent/', include('project_agent.urls')),
+    path('api/agent/', include('project_agent.urls')),
 ]
